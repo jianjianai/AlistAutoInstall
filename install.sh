ee42829a03c0bfde326d44aba2ef6c530c5fdd31
简简爱 <59829816+jianjianai@users.noreply.github.com>
2023-04-18 03:17:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-rm alist-linux-amd64.tar.gz
-wget https://github.com/alist-org/alist/releases/latest/download/alist-linux-amd64.tar.gz
-tar -zxvf alist-linux-amd64.tar.gz
-rm alist-linux-amd64.tar.gz
+rm alist-linux-musl-amd64.tar.gz
+wget https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz
+tar -zxvf alist-linux-musl-amd64.tar.gz
+rm alist-linux-musl-amd64.tar.gz
+chmod +x alist
 
 echo FROM scratch > Dockerfile
-echo ADD ./ ./ >> Dockerfile
+echo ADD ./ / >> Dockerfile
 echo EXPOSE 5244 >> Dockerfile
-echo CMD [ \"./alist\", \"server\"] >> Dockerfile
+echo CMD [\"/alist\",\"server\",\"--no-prefix\"] >> Dockerfile
 
 ./alist admin
 ./alist server
